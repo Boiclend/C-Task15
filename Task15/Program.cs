66283d780eb92b6106c23310e97579bc30bd9697
Roman <user@email.com>
2023-12-12 18:11:45 +0500

    added task15

---
+++ Task15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+int Message(string text) { 
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+void addRightDigit() { // функция добавления числа
+    int k = Message("k: ");
+        if(k <= 0) {
+            Console.WriteLine("Неверный формат числа");
+            return;
+        }
+    int d = Message("d: ");
+        if(d < 0 || d > 9) {
+            Console.WriteLine("Неверный формат числа");
+            return;
+        }
+    int firstNumber = int.Parse(k.ToString() + d.ToString()); // добавление первой цифры к числу
+    Console.WriteLine(firstNumber);
+    d = Message("d: ");
+        if(d < 0 || d > 9) {
+            Console.WriteLine("Неверный формат числа");
+            return;
+        }
+    int secondNumber = int.Parse(firstNumber.ToString() + d.ToString()); // добавление первой цифры к числу
+    Console.WriteLine(secondNumber); 
+}
+
+addRightDigit(); // вызов функции
